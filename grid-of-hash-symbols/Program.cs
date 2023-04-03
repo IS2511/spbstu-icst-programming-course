0207f736d3fdad1a4d3662c311ee8f875c3d4575
IS2511 <is2511@bk.ru>
2023-04-03 21:21:45 +0300

    feat(project): add grid-of-hash-symbols project

---
+++ grid-of-hash-symbols/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        const int grid_width = 5, grid_height = 4;
+        const int segment_size = 5;
+
+        for (int i = 0; i < grid_height * segment_size; i++)
+        {
+            for (int j = 0; j < grid_width * segment_size; j++)
+            {
+                if ( ((i + segment_size/2 + 1) % segment_size == 0)
+                     || ((j + segment_size / 2 + 1) % segment_size == 0) )
+                    Console.Write("#");
+                else
+                    Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+        
+    }
+}
