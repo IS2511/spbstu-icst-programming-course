543b4533a213b0f9edc2de8bcc7bd9e5a4a220eb
IS2511 <is2511@bk.ru>
2023-04-04 13:03:58 +0300

    feat(project): add find-n-biggest project

---
+++ find-n-biggest/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Security.Cryptography;
+
+internal class Program
+{
+    private static uint PowOfTwo(int power)
+    {
+        return 1u << power;
+    }
+    
+    private static uint GetDivision(uint index, uint length)
+    {
+        if (index == 0)
+            return length / 2;
+
+        return Math.Max(1, length / PowOfTwo(Convert.ToInt32(index + 1)));
+    }
+    
+    public static void Main(string[] args)
+    {
+        const uint N = 5;
+        const uint array_length = 20;
+
+        Console.Write("Array: [ ");
+        var array = new uint[array_length];
+        for (uint i = 0; i < array_length; i++)
+        {
+            array[i] = Convert.ToUInt32(RandomNumberGenerator.GetInt32(0, 1000));
+            Console.Write($"{array[i]} ");
+        }
+        Console.WriteLine("]");
+
+        var biggest = new SortedSet<uint>();
+        // var biggest = new uint[N];
+        for (uint i = 0; i < array_length; i++)
+        {
+            if ((i < N) || (array[i] > biggest.Min))
+            {
+                // Console.WriteLine($"+ {array[i]}");
+                biggest.Add(array[i]);
+
+                if (i >= N)
+                {
+                    // Console.WriteLine($"- {biggest.Min}");
+                    biggest.Remove(biggest.Min);
+                }
+            }
+        }
+
+        Console.Write("Biggest N values: [ ");
+        foreach (var value in biggest)
+        {
+            Console.Write($"{value} ");
+        }
+        Console.WriteLine("]");
+    }
+}
